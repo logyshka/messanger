3433e56f596564aebc2896080f8752aa5bb24b99
logyshka <kfirddsa@gmail.com>
2025-04-16 20:11:29 +0300

    Add chat and message models

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    text = models.CharField(max_length=2048)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(null=True)
