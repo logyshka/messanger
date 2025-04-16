ae8dddc63eae14dbf13db49ed34873c31e20bcd2
logyshka <kfirddsa@gmail.com>
2025-04-16 20:42:51 +0300

    Add create chat API method

--- chats/urls.py
+++ chats/urls.py
@@ -4,6 +4,7 @@ from chats import views
 
 api_urlpatterns = [
     path('chats/', views.ChatListView.as_view(), name='chats-list'),
+    path('chats/create/', views.ChatCreateView.as_view(), name='chat-create'),
     path('chats/<uuid:chat_id>/messages/', views.ChatMessagesView.as_view(), name='chat-messages'),
     path('chats/<uuid:chat_id>/messages/<int:pk>/', views.MessageDetailView.as_view(), name='message-detail'),
     path('chats/<uuid:chat_id>/sendMessage/', views.SendMessageView.as_view(), name='message-send'),
